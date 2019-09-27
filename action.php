c0e04e258b2394fcc95a6d30e0cd53c72440a343
Cesar Dasseville <55467162+Cesunga@users.noreply.github.com>
2019-09-26 21:01:39 -0300

    Add files via upload

---
+++ action.php
@@ -0,0 +1,15 @@
+<?php
+    echo "datos que fueron capturados: <br />";
+
+    $name = $_POST['name'];
+    echo $name . "<br />";
+    
+    $lastname = $_POST ['lastname'];
+    echo $lastname . "<br />";
+    
+    $mail = $_POST['mail'];
+    echo $mail . "<br />";
+    
+    $pass = $_POST['pass'];
+    echo $pass
+?>
